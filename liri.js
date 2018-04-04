e69b317142d7ea5066c8c8b88f7e1151184c0220
Daniel Foster <danieljfoster87@gmail.com>
2018-04-03 21:29:20 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+var spotifyApi = require("node-spotify-api");
+
+// get the code from the npm spotify page
+
+// id: process.env.SPOTIFYID,
+// secret: process.env.SPOTIFYSECRET
