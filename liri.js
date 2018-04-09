7c51bf6619913899fd9d5a4d4c08c4a504c4a8ba
Daniel Foster <danieljfoster87@gmail.com>
2018-04-09 18:09:43 -0400

    npm api info

--- liri.js
+++ liri.js
@@ -1,8 +1,49 @@
 require("dotenv").config();
 
-var spotifyApi = require("node-spotify-api");
+//-----------------------------------------
+// spotify 
 
-// get the code from the npm spotify page
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify(keys.spotify);
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
 
-// id: process.env.SPOTIFYID,
-// secret: process.env.SPOTIFYSECRET
+
+//-----------------------------------------
+// require
+
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+
+//-----------------------------------------
+// twitter
+
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
