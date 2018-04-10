2cfd855c151edb478981ccc2a87231464dbd82d6
Daniel Foster <danieljfoster87@gmail.com>
2018-04-10 17:31:31 -0400

    spotify functioning

--- liri.js
+++ liri.js
@@ -1,8 +1,12 @@
+
 require("dotenv").config();
 
 var keys = require("./keys");
+
+
 //-----------------------------------------
 // spotify
+
 if (process.argv[2] === "spotify-this-song") {
   var nodeArgs = process.argv;
 
@@ -24,27 +28,35 @@ if (process.argv[2] === "spotify-this-song") {
     if (err) {
       return console.log("Error occurred: " + err);
     } else {
-      // var artist = .artists.name;
-      // var songName = ;
-      // var preview = ;
-      // var album = .name;
-      // console.log("===========================================");
-      // console.log("Artist: " + artist + );
-      // console.log("===========================================");
+      var artist = data.tracks.items[0].artists[0].name;
+      var songName = data.tracks.items[0].name;
+      var preview = data.tracks.items[0].preview_url;
+      var album = data.tracks.items[0].album;
+
+      console.log("===========================================");
+
+      console.log("Artist: " + artist + 
+      "\nSong: " + songName + 
+      "\nPreview Link: " + preview +
+      "\nAlbum: " + album);
+
+      console.log("===========================================");
+
+      // console.log(JSON.stringify(data, null, 2));
+
     }
 
-    console.log(data.tracks.album.artists.name);
+    
   });
-
 
 } else if (process.argv[2] === "movie-this") {
   //-----------------------------------------
   // require
+
   var nodeArgs = process.argv;
-  // Create an empty variable for holding the movie name
+
   var movieName = "";
-  // Loop through all the words in the node argument
-  // And do a little for-loop magic to handle the inclusion of "+"s
+
   for (var i = 3; i < nodeArgs.length; i++) {
     if (i > 3 && i < nodeArgs.length) {
       movieName = movieName + "+" + nodeArgs[i];
@@ -55,13 +67,13 @@ if (process.argv[2] === "spotify-this-song") {
 
   var request = require("request");
 
-  var omdb = process.env.OMDB_API_KEY
-  
+  var omdb = process.env.OMDB_API_KEY;
+
   request(
     "http://www.omdbapi.com/?t=" + movieName + "&apikey=" + omdb,
     function(error, response, body) {
       if (error) {
-        console.log("error:", error); // Print the error if one occurred
+        console.log("error:", error);
       } else if (!error && response.statusCode === 200) {
         var title = JSON.parse(body).Title;
         var date = JSON.parse(body).Released;
@@ -71,7 +83,7 @@ if (process.argv[2] === "spotify-this-song") {
           rtRating = "no rating";
         } else {
           rtRating = JSON.parse(body).Ratings[1].Value;
-        };
+        }
         var country = JSON.parse(body).Country;
         var language = JSON.parse(body).Language;
         var plot = JSON.parse(body).Plot;
@@ -102,9 +114,13 @@ if (process.argv[2] === "spotify-this-song") {
       }
     }
   );
+
+
 } else if (process.argv[2] === "my-tweets") {
+
   //-----------------------------------------
   // twitter
+
   var Twitter = require("twitter");
 
   var client = new Twitter(keys.twitter);
@@ -116,12 +132,11 @@ if (process.argv[2] === "spotify-this-song") {
     response
   ) {
     if (!error) {
-
       console.log("===========================================");
 
       var myTweets = JSON.parse(tweets[0]).text;
 
-      console.log(myTweets);
+      console.log(JSON.stringify(myTweets, null, 2));
     }
   });
 }
@@ -129,4 +144,6 @@ if (process.argv[2] === "spotify-this-song") {
 //-----------------------------------------
 // do what it says
 if (process.argv[2] === "do-what-it-says") {
+
 }
+
